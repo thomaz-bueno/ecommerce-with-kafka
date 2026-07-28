c961ca55f88295e7b2f79564d1bed1e9adc4e9ca
thomaz.bueno <thomaz.bueno@unesp.br>
2026-07-28 15:14:51 -0300

    feat: add cors in api

--- orders-api/src/app.js
+++ orders-api/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const ordersRouter = require('./routes/orders.routes.js');
 const productsRouter = require('./routes/products.routes.js');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/health', (_request, response) => {
